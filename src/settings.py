50f4655b402f4c01a3eeaa01682e912dc1a547b2
erastov-alex <erastov.alex@gmail.com>
2023-10-24 18:44:58 +0300

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Сonstruction Equipment"
 PROJECT_NAME_FULL: str = "Сonstruction Equipment"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
